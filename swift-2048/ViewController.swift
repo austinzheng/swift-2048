ede4cff5d543974b1af8ca292fda2563bd125448
Austin Zheng <austinzheng@gmail.com>
2014-06-03 02:35:29 -0700

    Initial Commit

---
+++ swift-2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-2048
+//
+//  Created by Austin Zheng on 6/3/14.
+//  Copyright (c) 2014 Austin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
