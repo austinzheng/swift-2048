c71dc7907044ca7604f41d96969875448b31f2ab
Austin Zheng <austinzheng@gmail.com>
2015-10-25 01:41:28 -0700

    Removing confusing "All rights reserved" verbiage from source code

--- swift-2048/Views/GameboardView.swift
+++ swift-2048/Views/GameboardView.swift
@@ -3,7 +3,7 @@
 //  swift-2048
 //
 //  Created by Austin Zheng on 6/3/14.
-//  Copyright (c) 2014 Austin Zheng. All rights reserved.
+//  Copyright (c) 2014 Austin Zheng. Released under the terms of the MIT license.
 //
 
 import UIKit
