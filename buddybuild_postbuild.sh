9cca379a85c58698bd9384f673dc9719efa39d7f
Dennis <dennispi@gmail.com>
2016-10-30 18:36:52 -0700

    trying to push to hockeycrap

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -11,11 +11,12 @@ cd swift-2048.app.dSYM
 echo "whats in here"
 ls -la 
 zip -r mydSYMs.app.dSYM.zip * 
-echo "the path here is" pwd 
+echo "the path here is" 
+pwd 
 echo "whats in here"
 ls -la 
 
 
 echo "Uploading to HockeyApp with command" 
 echo curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
-curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
+curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@$BUDDYBUILD_PRODUCT_DIR/Release-iphoneos/swift-2048.app.dSYM/mydSYMs.app.dsym.zip" -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
