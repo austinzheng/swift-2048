d42e65884958dea640e398131f2e52feef194fdb
Dennis <dennispi@gmail.com>
2016-10-30 17:50:33 -0700

    trying to push to hockeycrap

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-echo $BUDDYBUILD_IPA_PATH
+echo Here is the post clone step
+curl -F "ipa=$BUDDYBUILD_IPA_PATH" -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
+
