db00b0ea95db5b48d336ed400f79a99f82c180bc
Dennis <dennispi@gmail.com>
2016-11-04 16:06:40 -0700

    more futzing

--- swift-2048/AppDelegate.swift
+++ swift-2048/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
   var window: UIWindow?
-
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    Fabric.with([Crashlytics.self])
+
     return true
   }
+    
 
   func applicationWillResignActive(_ application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
