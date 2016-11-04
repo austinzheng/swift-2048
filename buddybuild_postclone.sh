037be4f5fc24fe6eda8668340e86065673dcdd03
Dennis <dennispi@gmail.com>
2016-11-04 14:44:30 -0700

    postclone

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-
-#defaults write com.apple.iphonesimulator DebugLogging -bool YES
-#defaults write com.apple.CoreSimulator DebugLogging -bool YES
-#defaults write com.apple.dt.Xcode iOSSimulatorLogLevel 3
+echo Hello World
+echo $BUDDYBUILD_IPA_PATH
