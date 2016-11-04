d677864e7a2e7771be7f052752ad91f1fa00bedd
Dennis <dennispi@gmail.com>
2016-11-04 16:59:32 -0700

    fix

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -23,8 +23,6 @@ echo curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@$BUDDYBUILD_PRODUCT_DIR/Relea
 curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@$BUDDYBUILD_PRODUCT_DIR/Release-iphoneos/swift-2048.app.dSYM/mydSYMs.app.dsym.zip" -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
 
 cd $BUDDYBUILD_PRODUCT_DIR 
-echo "whats in here Release-iphoneos"
-ls -la
-cd Release-iphoneos
+echo "whats in here"
 ls -lR
 #find . -name "*.dSYM" | xargs -I \{\} Pods/Fabric/upload-symbols -a 4e398883f5a51079e0cfd9aa715683d57ac5a235 -p ios
