853480f9395059a6c88d1146c14b32be6ed8a0bc
Dennis <dennispi@gmail.com>
2016-11-06 07:28:56 -0800

    vars

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -21,15 +21,20 @@ cd $BUDDYBUILD_PRODUCT_DIR
 find . -name "*.dSYM"
 
 
-echo -e "\n Uploading to HockeyApp via command line" 
-cd $BUDDYBUILD_PRODUCT_DIR 
-find . -name "*.dSYM" | xargs -I \{\} curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@" \{\} -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
+#echo -e "\n Uploading to HockeyApp via command line" 
+#cd $BUDDYBUILD_PRODUCT_DIR 
+#find . -name "*.dSYM" | xargs -I \{\} curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@" \{\} -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
+
+echo "Uploading to HockeyApp via command line"
+HOCKEYAPP_API_TOKEN = '3c2b4783db2447518590a3a7d946ab67'
+HOCKEYAPP_APP_ID = '9934149eb72b4e6ab617feb1d822dae0'
+
+cd $BUDDYBUILD_PRODUCT_DIR
+find . -name "*.dSYM" | xargs -I \{\} curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@" \{\} -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
+
 
 
 #curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@$BUDDYBUILD_PRODUCT_DIR/Release-iphoneos/swift-2048.app.dSYM/mydSYMs.app.dsym.zip" -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
 
 echo -e "\nUploading to Fabric via command line" 
-
-cd $BUDDYBUILD_PRODUCT_DIR 
-#find . -name "*.dSYM" | xargs -I \{\} $BUDDYBUILD_WORKSPACE/Pods/Fabric/upload-symbols -a 4e398883f5a51079e0cfd9aa715683d57ac5a235 -p ios \{\}
 $BUDDYBUILD_WORKSPACE/Pods/Fabric/upload-symbols -a 4e398883f5a51079e0cfd9aa715683d57ac5a235 -p ios $BUDDYBUILD_PRODUCT_DIR 
