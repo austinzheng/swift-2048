ea91afbf54e954299ea830fb38b8ea28d0e36f06
Dennis <dennispi@gmail.com>
2016-11-07 15:00:58 -0800

    disable for a second

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Example for adding a key to the Plist
-cd $BUDDYBUILD_WORKSPACE
-ls -laR
-echo password | sudo -S gem install slather
-slather coverage -s --scheme swift-2048 $BUDDYBUILD_WORKSPACE/swift-2048.xcodeproj
+#cd $BUDDYBUILD_WORKSPACE
+#ls -laR
+#echo password | sudo -S gem install slather
+#slather coverage -s --scheme swift-2048 $BUDDYBUILD_WORKSPACE/swift-2048.xcodeproj
