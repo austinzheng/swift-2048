9436848ec950b6f1ac022832a912e011d7e88ea8
Dennis <dennispi@gmail.com>
2016-11-08 06:38:50 -0800

    looking for apps

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+cd $BUDDYBUILD_TEST_DIR
+echo "Whats in $BUDDYBUILD_TEST_DIR"
+ls -laR
+
 # GET DSYMS
 #echo "Generating dSYMs" 
 #cd $BUDDYBUILD_PRODUCT_DIR 
@@ -16,31 +21,31 @@
 #echo "whats in here"
 #ls -la 
 
-echo -e "\n Where Da Syms" 
-cd $BUDDYBUILD_PRODUCT_DIR 
-find . -name "*.dSYM"
+#echo -e "\n Where Da Syms" 
+#cd $BUDDYBUILD_PRODUCT_DIR 
+#find . -name "*.dSYM"
 
 
 #echo -e "\n Uploading to HockeyApp via command line" 
 #cd $BUDDYBUILD_PRODUCT_DIR 
 #find . -name "*.dSYM" | xargs -I \{\} curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@" \{\} -H "X-HockeyAppToken: 3c2b4783db2447518590a3a7d946ab67" https://rink.hockeyapp.net/api/2/apps/9934149eb72b4e6ab617feb1d822dae0/app_versions/upload
 
-echo "Uploading to HockeyApp via command line"
+#echo "Uploading to HockeyApp via command line"
 
-if [[ "$BUDDYBUILD_BRANCH" =~ "release" ]]; then
-    HOCKEYAPP_API_TOKEN=3c2b4783db2447518590a3a7d946ab67
-    HOCKEYAPP_APP_ID=9934149eb72b4e6ab617feb1d822dae0
+#if [[ "$BUDDYBUILD_BRANCH" =~ "release" ]]; then
+ #   HOCKEYAPP_API_TOKEN=3c2b4783db2447518590a3a7d946ab67
+ #   HOCKEYAPP_APP_ID=9934149eb72b4e6ab617feb1d822dae0
 
-    cd $BUDDYBUILD_PRODUCT_DIR
-    find . -name "*.dSYM" | xargs -I \{\} curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@" \{\} -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
-else
-    echo "This wasn't a release branch!"
-fi
+#    cd $BUDDYBUILD_PRODUCT_DIR
+#    find . -name "*.dSYM" | xargs -I \{\} curl -F "ipa=@$BUDDYBUILD_IPA_PATH" -F "dsym=@" \{\} -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
+#else
+ #   echo "This wasn't a release branch!"
+ #fi
 
-echo "Trying Slather"
+#echo "Trying Slather"
 
-echo password | sudo -S gem install slather
-slather coverage -s --scheme swift-2048 --build-directory $BUDDYBUILD_TEST_DIR/Build/Intermediates $BUDDYBUILD_WORKSPACE/swift-2048.xcodeproj
+#echo password | sudo -S gem install slather
+#slather coverage -s --scheme swift-2048 --build-directory $BUDDYBUILD_TEST_DIR/Build/Intermediates $BUDDYBUILD_WORKSPACE/swift-2048.xcodeproj
 
 
 
