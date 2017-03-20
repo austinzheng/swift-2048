3fd9cdef374a21c768482c1a4f14bfb357240f1c
buddybuild <help@buddybuild.com>
2017-03-19 22:54:40 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

---
+++ Bridging-Header.h
@@ -0,0 +1,1 @@
+#import <BuddyBuildSDK/BuddyBuildSDK.h>
--- swift-2048/AppDelegate.swift
+++ swift-2048/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    BuddyBuildSDK.setup()
+    
 
     return true
   }
