642a74e95b44a9aea50664e82d8e237391e54a34
Christopher Stott <christopherstott@gmail.com>
2017-03-20 17:27:46 -0700

    Add the new SDK

--- swift-2048/AppDelegate.swift
+++ swift-2048/AppDelegate.swift
@@ -42,5 +42,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
+    
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        BuddyBuildSDK.uiTestRecordingDidReceiveRemoteNotification(userInfo)
+    }
 }
 
